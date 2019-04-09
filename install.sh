8b247c0c1c0e3c040c66991455103111bc8ea86e
David R Connell <davidconnell12@gmail.com>
2019-04-09 16:38:05 -0500

    Refactoring, move bin dir

--- install.sh
+++ install.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-TEXLOC=$(kpsewhich -expand-var '$TEXMFHOME') || false
-BINLOC=~/bin
-
-function missinglatex {
-	cat <<- _EOF_
-		Package requires LaTeX to be installed
-		_EOF_
-
-	exit 1
-}
-
-[ -d $TEXLOC ] || missinglatex
+source .setuptools/linklocations
 [ -d $TEXLOC/tex/latex ] || mkdir -p $TEXLOC/tex/latex
-[ -d $BINLOC ] || mkdir ~/bin
 
-
-for f in $PWD/packages/*; do
-	ln -s $f $TEXLOC/tex/latex/${f#*packages/}
-done
-
-for f in $PWD/bin/*; do
-	ln -s $f $BINLOC/${f#*bin/}
-done
-
-./.setuptools/gendocsrc.sh
+.setuptools/lncontents.sh packages $TEXDIR
+.setuptools/lncontents.sh bin $BINDIR
+.setuptools/gendocsrc.sh
